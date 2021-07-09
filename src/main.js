a8664bffc45490e936690662ff9530dac2310ac7
Orest Mamchuk <omamc@Orests-Mac-mini.local>
2021-07-09 19:40:14 +0300

    wip

--- src/main.js
+++ src/main.js
@@ -1,13 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-// import GSignInButton from 'vue-google-signin-button'
-
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@/assets/css/main.css'
-//
-// Vue.config.productionTip = false
-// import GoogleAuth from 'vue-google-auth'
 
 new Vue({
     router,
@@ -15,6 +10,3 @@ new Vue({
     render: h => h(App),
 }).$mount('#app')
 
-//
-// Vue.use(GoogleAuth, { client_id: '953128976338-aps8nh9i79cmqgv9m29h3bcd4r9tpbuh.apps.googleusercontent.com' })
-// Vue.googleAuth().load()
